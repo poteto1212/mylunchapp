1b7817a4ff2e7ccd07ca28edd8e5b3a54d4c0fda
Your Name <you@example.com>
2021-03-10 08:46:52 +0000

    init

--- lunchmap/views.py
+++ lunchmap/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Category, Shop
 
-# Create your views here.
+#一覧ページの元となるクラス
+#ListViewによってモデルのページを一覧ページにする事が出来る
+class IndexView(generic.ListView):
+    model=Shop
+    
+    
