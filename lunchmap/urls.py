9f3df76eb70002c25239108ee293b94c546fe27c
Your Name <you@example.com>
2021-03-12 02:36:02 +0000

    init

--- lunchmap/urls.py
+++ lunchmap/urls.py
@@ -7,5 +7,5 @@ urlpatterns=[
     path('',views.IndexView.as_view(),name='index'),
     path('<int:pk>/',views.DetailView.as_view(),name='detail'),
     path('create/',views.CreateView.as_view(),name='create'),
-    path('<int:pk>/update',views.CreateView.as_view(),name='update'),
+    path('<int:pk>/update/',views.UpdateView.as_view(),name='update'),
 ]
