fa06980d1ace56aab011d529d87303983c8b96cc
Your Name <you@example.com>
2021-03-12 09:18:45 +0000

    init

--- lunchmap/views.py
+++ lunchmap/views.py
@@ -1,7 +1,9 @@
 from django.views import generic
 from django.urls import reverse_lazy
 from .models import Category, Shop
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+# LoginRequiredMixinを引数に含むクラスはログイン時のみアクセスできる
 
 #一覧ページの元となるクラス
 #ListViewによってモデルのページを一覧ページにする事が出来る
@@ -16,20 +18,31 @@ class DetailView(generic.DetailView):
 
 #更新機能の元となるクラス
 #CreateVieはデータモデルを作成する為のクラス
-#fields="__カラムの範囲＿"　今回は全てのカラムを入力対象にしている
-class CreateView(generic.edit.CreateView):
+#fields対象はリスト内のみ　
+class CreateView(LoginRequiredMixin,generic.edit.CreateView):
     model=Shop
-    fields='__all__'
+    fields=['name','address','category']
+    
+    #ログインユーザー名を投稿者名に代入する処理を含んだ関数を定義
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(CreateView, self).form_valid(form)
 
 #データモデルを更新する為のクラスベースビュー
 #範囲はクリエイトと同じ
-class UpdateView(generic.edit.UpdateView):
+class UpdateView(LoginRequiredMixin,generic.edit.UpdateView):
     model=Shop
-    fields='__all__'
+    fields = ['name', 'address', 'category'] # '__all__'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied('You do not have permission to edit.')
+        return super(UpdateView, self).dispatch(request, *args, **kwargs)
     
 #データベース削除の為のクラスベースビュー
 # success_url = reverse_lazy('lunchmap:index')でリダイレクト先のURLを調べる
 
-class DeleteView(generic.edit.DeleteView):
+class DeleteView(LoginRequiredMixin,generic.edit.DeleteView):
     model=Shop
     success_url = reverse_lazy('lunchmap:index')
